34a666df64bc96cad33055a84a67617f76d21b07
sky_6776 <137915295+sky6776@users.noreply.github.com>
2023-09-20 19:54:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ window.addEventListener("load", function () {
         document.querySelector("main").style.display = "block";
     }, 5000);
 });
+
+window.addEventListener("load", function () {
+    var loadingElement = document.getElementById("loading");
+    var overlayElement = document.getElementById("overlay");
+    var mainElement = document.querySelector("main");
+    var darkModeButton = document.getElementById("dark-mode-toggle");
+
+    setTimeout(function () {
+        loadingElement.style.display = "none";
+        overlayElement.style.display = "none";
+        mainElement.style.display = "block";
+    }, 5000);
+
+    darkModeButton.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+});
